5aee8c9e8387aa7cda682d3799c1311a8b0d4fe1
GuillermoPerez32 <luisguillermo.rodriguez32@gmail.com>
2022-07-17 20:02:23 -0400

    Updated Detail View

--- lib/presentation/pages/product_detail.dart
+++ lib/presentation/pages/product_detail.dart
@@ -12,31 +12,105 @@ class ProductDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size _screenSize = MediaQuery.of(context).size;
     return Scaffold(
       body: ListView(
         children: [
           Stack(
             children: [
-              Column(
-                children: [
-                  Hero(
-                    child: Image(image: AssetImage(imgRoute)),
-                    tag: id,
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    icon: const Icon(Icons.arrow_back),
-                  ),
-                ],
-              )
+              _Body(imgRoute: imgRoute, screenSize: _screenSize, id: id),
+              const _AppBar(),
+              _LikeButton(screenSize: _screenSize),
             ],
           ),
         ],
       ),
+    );
+  }
+}
+
+class _LikeButton extends StatelessWidget {
+  const _LikeButton({
+    Key? key,
+    required Size screenSize,
+  })  : _screenSize = screenSize,
+        super(key: key);
+
+  final Size _screenSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: _screenSize.width * .8,
+      height: _screenSize.height,
+      child: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.thumb_up_alt_outlined,
+          color: Colors.grey,
+        ),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({
+    Key? key,
+    required this.imgRoute,
+    required Size screenSize,
+    required this.id,
+  })  : _screenSize = screenSize,
+        super(key: key);
+
+  final String imgRoute;
+  final Size _screenSize;
+  final String id;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Hero(
+          child: ClipRRect(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(
+                80,
+              ),
+            ),
+            child: Image(
+              image: AssetImage(imgRoute),
+              height: _screenSize.height * .5,
+              width: _screenSize.width,
+              fit: BoxFit.cover,
+            ),
+          ),
+          tag: id,
+        ),
+        SizedBox(
+          height: _screenSize.height * .5,
+        )
+      ],
+    );
+  }
+}
+
+class _AppBar extends StatelessWidget {
+  const _AppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ],
     );
   }
 }
